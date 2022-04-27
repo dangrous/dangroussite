260f33a1123d886f15c137a2d1d59fc3355430e8
dangrous <5487802+dangrous@users.noreply.github.com>
2022-04-26 19:27:46 -0700

    starting to customize

---
+++ src/pages/test.js
@@ -0,0 +1,12 @@
+import * as React from 'react'
+
+const TestPage = () => {
+  return (
+    <div>
+      <h1 className='p-5 text-xl'>HELLO THIS IS A TEST</h1>
+      <a href='/'>go home</a>
+    </div>
+  )
+}
+
+export default TestPage

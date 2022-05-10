9ac287a8d2109d5fb84bb881ccfee54384729808
dangrous <5487802+dangrous@users.noreply.github.com>
2022-05-10 12:20:54 -0700

    add favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   siteMetadata: {
-    title: `Dangro.us`,
-    siteUrl: `https://www.yourdomain.tld`,
+    title: `DANGRO.US`,
+    siteUrl: `https://dangro.us`,
   },
   plugins: [
     {
@@ -30,6 +30,24 @@ module.exports = {
         path: './src/pages/',
       },
       __key: 'pages',
+    },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: 'DANGRO.US',
+        short_name: 'DANGRO.US',
+        start_url: '/',
+        background_color: '#6b37bf',
+        theme_color: '#6b37bf',
+        // Enables "Add to Homescreen" prompt and disables browser UI (including back button)
+        // see https://developers.google.com/web/fundamentals/web-app-manifest/#display
+        display: 'standalone',
+        icon: 'src/images/icon.jpg', // This path is relative to the root of the site.
+        // An optional attribute which provides support for CORS check.
+        // If you do not provide a crossOrigin option, it will skip CORS for manifest.
+        // Any invalid keyword or empty string defaults to `anonymous`
+        crossOrigin: `use-credentials`,
+      },
     },
   ],
 }

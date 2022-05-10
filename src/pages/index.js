2e96313e36e77757a86257d80a3a305116c1925d
dangrous <5487802+dangrous@users.noreply.github.com>
2022-05-10 13:21:30 -0700

    add react helmet and therefore site title

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,6 @@
 import * as React from 'react'
 import { StaticImage } from 'gatsby-plugin-image'
+import { Helmet } from 'react-helmet'
 import resume from '../files/DGRresume.pdf'
 
 const projects = [
@@ -68,112 +69,120 @@ const PortfolioProject = ({ project }) => {
 
 const IndexPage = () => {
   return (
-    <main className='flex bg-gradient-to-br from-slate-300 to-slate-50 flex-col min-h-screen border border-slate-500 border-8 '>
-      {/* <StaticImage
+    <>
+      <Helmet>
+        <title>DANGRO.US</title>
+      </Helmet>
+      <main className='flex bg-gradient-to-br from-slate-300 to-slate-50 flex-col min-h-screen border border-slate-500 border-8 '>
+        {/* <StaticImage
         src='../images/header.jpeg'
         alt='A man standing in front of Yosemite Valley'
         height='400'
       /> */}
-      <div
-        id='mainstuff'
-        className='flex container flex-col flex-grow mx-3 mt-3 lg:mt-0'
-      >
-        <div className='flex flex-wrap lg:flex-nowrap items-center mb-8 mr-4'>
-          <StaticImage
-            src='../images/icon.jpg'
-            alt=''
-            className='md:w-20 md:h-20 mr-2'
-          />
-          <p className='text-8xl font-header text-slate-500 pt-3'>
-            Hi there, I'm Daniel Gale-Rosen.
-          </p>
-        </div>
+        <div
+          id='mainstuff'
+          className='flex container flex-col flex-grow mx-3 mt-3 lg:mt-0'
+        >
+          <div className='flex flex-wrap lg:flex-nowrap items-center mb-8 mr-4'>
+            <StaticImage
+              src='../images/icon.jpg'
+              alt=''
+              className='md:w-20 md:h-20 mr-2'
+            />
+            <p className='text-8xl font-header text-slate-500 pt-3'>
+              Hi there, I'm Daniel Gale-Rosen.
+            </p>
+          </div>
 
-        <div className='flex flex-col pt-2 mr-8'>
-          <div className='flex mb-3'>
-            <p className='lg:basis-1/2'>
-              I'm a full-stack developer with deep (and now somewhat useless)
-              marketing experience. I've been coding on the side for years,
-              primarily in JavaScript and Python, and I'm ready to move into the
-              professional software development world.
-            </p>
+          <div className='flex flex-col pt-2 mr-8'>
+            <div className='flex mb-3'>
+              <p className='lg:basis-1/2'>
+                I'm a full-stack developer with deep (and now somewhat useless)
+                marketing experience. I've been coding on the side for years,
+                primarily in JavaScript and Python, and I'm ready to move into
+                the professional software development world.
+              </p>
+            </div>
+            <div className='flex mb-3'>
+              <p className='lg:basis-1/2'>
+                I've always had a passion for creating, whether that means
+                designing an escape room, learning how to code or engineer
+                something new, or perfecting the Old Fashioned. I have a passion
+                for games, immersive experiences, and finding other ways to
+                break people out of their usual routine.
+              </p>
+            </div>
+            <div className='flex mb-3'>
+              <p className='lg:basis-1/2'>
+                If you'd like to talk about something you're working on or what
+                we could make together, let's chat.
+              </p>
+            </div>
           </div>
-          <div className='flex mb-3'>
-            <p className='lg:basis-1/2'>
-              I've always had a passion for creating, whether that means
-              designing an escape room, learning how to code or engineer
-              something new, or perfecting the Old Fashioned. I have a passion
-              for games, immersive experiences, and finding other ways to break
-              people out of their usual routine.
-            </p>
+          <a className='underline' href={resume} download>
+            Resume
+          </a>
+          <h2 className='text-2xl my-3'>Portfolio</h2>
+          <div className='flex flex-wrap mr-6'>
+            {projects.map((project) => {
+              return <PortfolioProject project={project} />
+            })}
           </div>
-          <div className='flex mb-3'>
-            <p className='lg:basis-1/2'>
-              If you'd like to talk about something you're working on or what we
-              could make together, let's chat.
-            </p>
-          </div>
-        </div>
-        <a className='underline' href={resume} download>
-          Resume
-        </a>
-        <h2 className='text-2xl my-3'>Portfolio</h2>
-        <div className='flex flex-wrap mr-6'>
-          {projects.map((project) => {
-            return <PortfolioProject project={project} />
-          })}
-        </div>
-        <h2 className='text-2xl my-3'>Other Endeavors</h2>
-        <ul className='list-disc list-inside'>
-          <li>
-            <a href='https://www.blackwoodcharter.org' className='underline'>
-              Escape Rooms
-            </a>
-          </li>
-          <li>
-            <a
-              href='https://52frames.com/photographer/13983'
-              className='underline'
-            >
-              Photography
-            </a>
-          </li>
-          <li>
-            <a href='https://www.boozynewsie.com' className='underline'>
-              Cocktails
-            </a>
-          </li>
-        </ul>
-      </div>
-      <footer className='flex p-5 bg-slate-500 mt-6 text-white justify-between'>
-        <div className='flex flex-col flex-grow-0 basis-1/4 mr-5'>
-          <h2 className='font-bold text-lg'>Contact and Links</h2>
-          <ul className='list-inside flex justify-between flex-grow-0'>
-            <a href='mailto:dangalerosen@gmail.com' className='underline'>
-              Email
-            </a>
-            <span>/</span>
-            <a href='https://github.com/dangrous/' className='underline'>
-              GitHub
-            </a>
-            <span>/</span>
-            <a
-              href='https://www.linkedin.com/in/dgalerosen/'
-              className='underline'
-            >
-              LinkedIn
-            </a>
-            <span>/</span>
-            <a href='https://www.instagram.com/dangrous/' className='underline'>
-              Instagram
-            </a>
+          <h2 className='text-2xl my-3'>Other Endeavors</h2>
+          <ul className='list-disc list-inside'>
+            <li>
+              <a href='https://www.blackwoodcharter.org' className='underline'>
+                Escape Rooms
+              </a>
+            </li>
+            <li>
+              <a
+                href='https://52frames.com/photographer/13983'
+                className='underline'
+              >
+                Photography
+              </a>
+            </li>
+            <li>
+              <a href='https://www.boozynewsie.com' className='underline'>
+                Cocktails
+              </a>
+            </li>
           </ul>
         </div>
-        <p className='self-end'>
-          This website is created with Gatsby.js and styled with Tailwind.css
-        </p>
-      </footer>
-    </main>
+        <footer className='flex p-5 bg-slate-500 mt-6 text-white justify-between'>
+          <div className='flex flex-col flex-grow-0 basis-1/4 mr-5'>
+            <h2 className='font-bold text-lg'>Contact and Links</h2>
+            <ul className='list-inside flex justify-between flex-grow-0'>
+              <a href='mailto:dangalerosen@gmail.com' className='underline'>
+                Email
+              </a>
+              <span>/</span>
+              <a href='https://github.com/dangrous/' className='underline'>
+                GitHub
+              </a>
+              <span>/</span>
+              <a
+                href='https://www.linkedin.com/in/dgalerosen/'
+                className='underline'
+              >
+                LinkedIn
+              </a>
+              <span>/</span>
+              <a
+                href='https://www.instagram.com/dangrous/'
+                className='underline'
+              >
+                Instagram
+              </a>
+            </ul>
+          </div>
+          <p className='self-end'>
+            This website is created with Gatsby.js and styled with Tailwind.css
+          </p>
+        </footer>
+      </main>
+    </>
   )
 }
 
